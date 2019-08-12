a39b39471f555170d47100746837345b44361cea
Satvik Kaul <satvik.1799@gmail.com>
2019-08-12 16:24:48 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,8 +1,9 @@
 # code your #valid_move? method here
 def valid_move?(board, index)
-  if position_taken?
+  if position_taken?(board, index)
     execute position_taken?
   else
+    return nil
 
 
 
